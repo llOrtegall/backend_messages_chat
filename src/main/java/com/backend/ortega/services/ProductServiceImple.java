0e8a07dae46a197876da4f7ba97b0f7677686c85
llortegall <IvanOrtega_97@hotmail.com>
2026-02-20 12:57:13 -0500

    feat: implement ProductServiceImple with CRUD operations and update ProductServices interface

--- src/main/java/com/backend/ortega/services/ProductServiceImple.java
+++ src/main/java/com/backend/ortega/services/ProductServiceImple.java
@@ -1,4 +1,44 @@
 package com.backend.ortega.services;
 
-public class ProductServiceImple {
+import com.backend.ortega.entities.Product;
+import com.backend.ortega.repositories.ProductRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ProductServiceImple implements ProductServices{
+    final private ProductRepository productRepo;
+
+    public ProductServiceImple(ProductRepository productRepo) {
+        this.productRepo = productRepo;
+    }
+
+    @Transactional(readOnly = true)
+    @Override
+    public List<Product> findAll() {
+        return  (List<Product>) productRepo.findAll();
+    }
+
+    @Transactional(readOnly = true)
+    @Override
+    public Optional<Product> findById(Long id) {
+        return productRepo.findById(id);
+    }
+
+    @Override
+    public Product save(Product product) {
+        return productRepo.save(product);
+    }
+
+    @Transactional()
+    @Override
+    public void deleteById(Long id) {
+        Optional<Product> product = productRepo.findById(id);
+        if(product.isPresent()) {
+            productRepo.deleteById(id);
+        }
+    }
 }
--- src/main/java/com/backend/ortega/services/ProductServices.java
+++ src/main/java/com/backend/ortega/services/ProductServices.java
@@ -1,4 +1,13 @@
 package com.backend.ortega.services;
 
+import com.backend.ortega.entities.Product;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface ProductServices {
+    List<Product> findAll();
+    Optional<Product> findById(Long id);
+    Product save(Product product);
+    void deleteById(Long id);
 }
