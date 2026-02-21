e68d8190d930ab5ccb9b2b7e19d965624c5b8fa0
llortegall <ivanortega_97@hotmail.com>
2026-02-21 18:33:26 -0500

    feat: add ProductMapper interface for mapping ProductEntity to ProductDTO

---
+++ src/main/java/com/backend/ortega/persistence/mapper/ProductMapper.java
@@ -0,0 +1,19 @@
+package com.backend.ortega.persistence.mapper;
+
+import java.util.List;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import com.backend.ortega.domain.dto.ProductDTO;
+import com.backend.ortega.persistence.entities.ProductEntity;
+
+@Mapper(componentModel = "spring")
+public interface ProductMapper {
+    
+    @Mapping(source = "name", target = "nombre")
+    @Mapping(source = "description", target = "descripcion")
+    @Mapping(source = "price", target = "precio")
+    ProductDTO toDto(ProductEntity entity);
+    List<ProductDTO> toDtos(Iterable<ProductEntity> entities);
+}
