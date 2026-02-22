65023ac9a8ee3436c1e336735553ad02aa36a090
llortegall <ivanortega_97@hotmail.com>
2026-02-22 00:59:54 -0500

    feat: enhance ProductMapper with entity conversion and update methods for ProductDTO

--- src/main/java/com/backend/ortega/persistence/mapper/ProductMapper.java
+++ src/main/java/com/backend/ortega/persistence/mapper/ProductMapper.java
@@ -2,8 +2,10 @@ package com.backend.ortega.persistence.mapper;
 
 import java.util.List;
 
+import org.mapstruct.InheritInverseConfiguration;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
 
 import com.backend.ortega.domain.dto.ProductDTO;
 import com.backend.ortega.persistence.entities.ProductEntity;
@@ -16,4 +18,9 @@ public interface ProductMapper {
     @Mapping(source = "price", target = "precio")
     ProductDTO toDto(ProductEntity entity);
     List<ProductDTO> toDtos(Iterable<ProductEntity> entities);
+    @InheritInverseConfiguration
+    ProductEntity toEntity(ProductDTO dto);
+    @InheritInverseConfiguration
+    @Mapping(target = "id", ignore = true)
+    void updateEntityFromDto(ProductDTO productDTO, @MappingTarget ProductEntity productEntity);
 }
