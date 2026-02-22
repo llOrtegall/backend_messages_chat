7b68b25cf0c58f1daf769e707067a38f4fead366
llortegall <ivanortega_97@hotmail.com>
2026-02-22 00:59:41 -0500

    feat: add methods for product retrieval and management in ProductRepositoryImplementation

--- src/main/java/com/backend/ortega/domain/repository/ProductRepositoryImplementation.java
+++ src/main/java/com/backend/ortega/domain/repository/ProductRepositoryImplementation.java
@@ -6,4 +6,7 @@ import com.backend.ortega.domain.dto.ProductDTO;
 
 public interface ProductRepositoryImplementation {
     List<ProductDTO> findAllProducts();
+    ProductDTO findById(Long id);
+    ProductDTO save(ProductDTO product);
+    ProductDTO update(Long id, ProductDTO updated);
 }
